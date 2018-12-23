3b695127aae5771cf690a6a90c5183a1b73efb4a
ProgammerMasya <vlad3032001@mail.ru>
2018-12-23 21:40:02 +0300

    Automatic Schedule Appeared

--- send_message_after_parser.py
+++ send_message_after_parser.py
@@ -1,14 +1,14 @@
 from consts import USERS_IMAGES_FULL
 from app.model.vk.vk import Vk
-from settings import config
+from settings import *
 from app.db.db import DB
 
 
 db_obj = DB(
-    "dbname='Masya' "
-    "user='postgres' "
-    "host='localhost' "
-    "password='vlad3052001'"
+    "dbname=masya' "
+    f"user='{config['db_user']}' "
+    f"host='{config['db_host']}' "
+    f"password='{config['db_password']}"
 )
 
 vk_obj = Vk(config['token'])
