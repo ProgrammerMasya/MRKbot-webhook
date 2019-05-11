8286f64a02384a0c79dace9bc6052ff8d7405dd2
ProgammerMasya <vlad3032001@mail.ru>
2019-05-11 12:24:07 +0300

    changes

--- app/model/vk/vk.py
+++ app/model/vk/vk.py
@@ -5,5 +5,5 @@ class Vk:
     def __init__(self, token):
         self.token = token
 
-    def send_message(self, user_id, message, attachment, file):
+    def send_message(self, user_id, message, attachment, file=None):
         vkapi.send_message(user_id, self.token, message, attachment, file)
