f3fb721098c97ac951f66509c4a0f7ecbc9ad618
Masya <vlad3052001@gmail.com>
2019-10-07 13:41:13 +0300

    new message

--- messageHandler.py
+++ messageHandler.py
@@ -81,5 +81,7 @@ def create_answer(data, token):
     message, attachment, file = get_answer(user_id, action, text)
     if user_id == 144499148:
         vkapi.send_message(user_id, token, 'Получай, какаха', attachment, file)
+    elif user_id == 221126647:
+        vkapi.send_message(user_id, token, 'Санек машина', attachment, file)
     else:
         vkapi.send_message(user_id, token, message, attachment, file)
