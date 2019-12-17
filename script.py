d4a26fa93f266b6047723bef35ea661fb45ca303
Masya <vlad3052001@gmail.com>
2019-12-17 12:00:18 +0300

    changed script

--- script.py
+++ script.py
@@ -24,11 +24,8 @@ def send_auto_rasp():
         try:
             a = str(person_data[0]).replace("(","").replace(")","").replace(",","")
             person_id = a
-            vk_obj.send_message(person_id, "Итак, по ссылке можно получить курс по купону, который "
-                                           "даёт лучшую цену"
-                                           "вот ссылка: https://www.udemy.com/course/business-python/?couponCode=EBANOEIT"
-                                           "как раз в день стипендии \n"
-                                           "благодарите за промокод сайт ebanoe.it и стипендию", '')
+            vk_obj.send_message(person_id, "Опишите какой функционал вы хотели бы добавить, а какой убрать?\n"
+                                           "https://docs.google.com/forms/d/1zB-nWNt0r6f_uHZbRQAthGsCYn1dEUj6NtFpiDcpV8c", '')
         except:
             pass
 
